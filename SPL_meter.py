95dd4e44982a2fee110c483fa15211a3927f2961
L0ranos <piotr.ksiazek250@gmail.com>
2024-03-15 07:06:33 +0100

    Made program work, ready fo setup with real raspi

---
+++ SPL_meter.py
@@ -0,0 +1,95 @@
+from time import sleep
+from queue import Empty, Queue
+import sounddevice as sd
+import numpy as np
+from A_weighting import A_weighting 
+from numpy import mean, square, log10, percentile, frombuffer, sqrt
+import os
+from scipy.signal import lfilter
+
+class SPL_Meter_Manager:
+    """This is the class responsible for recording sound levels and passing them to a queue.
+    """ 
+    l_dbs = []
+    a_dbs = []
+    reference_level = 94
+    calibration = 0
+    sampling_rate = 48000
+    sensitivity = -26
+    timeweighting = 0.125
+
+    frame_counter=0
+    max_frame_count = 0
+
+    def __init__(self, sound_config) -> None:
+        #ładowanie ustawień
+        self.reference_level = float(sound_config.get("calibration_level", 94))
+        self.calibration = float(sound_config.get("calibration_correction_value", 0))
+        self.sensitivity = float(sound_config.get("sensitivity_dbfs", -26))
+        self.sampling_rate = int(sound_config.get("sample_rate", 48000))
+        self.timeweighting = float(sound_config.get("timeweighting", 0.125)) #domyślnie stała czasowa fast
+
+        #zerowanie wyników
+        self.max_frame_count = int(60*15*1/self.timeweighting) # zakladam max czas usredniania jako 15 minut
+        self.l_dbs = []
+        self.a_dbs = []
+
+    def callback(self, indata, frames, time, status):
+        if any(indata):
+            max_lvl = self.reference_level - self.sensitivity
+
+            data_input=indata[:,0]
+            l_data = data_input - np.mean(data_input)
+
+            b, a = A_weighting(self.sampling_rate)
+            a_data = lfilter(b, a, l_data)
+
+            l_db = max_lvl + 20*np.log10(np.mean(np.sqrt(l_data**2))) + self.calibration
+            a_db = max_lvl + 20*np.log10(np.mean(np.sqrt(a_data**2))) + self.calibration
+
+            self.l_dbs.append(l_db)
+            self.a_dbs.append(a_db)
+        
+            if(len(self.l_dbs)>=self.max_frame_count): 
+                self.l_dbs.pop(0)
+                self.a_dbs.pop(0)       
+        else:
+            print('no input')
+
+    # def save_csv(self, measurement_time, leq, laeq):
+    #     path = os.path.join(self.path,"SPL_"+str(measurement_time.date())+".csv")
+    #     f_object = open(path, 'a')
+    #     f_object.write("{},{:.2f},{:.2f}\n".format(
+    #                 measurement_time, leq,  laeq))
+    #     f_object.close()
+
+    def measure_SPL(self, spl_queue, do_reset_queue):
+        CHANNEL = 1 #To NIE zmienia się w naszej konfiguracji. Jeśli zmienił się model mikrofonu i nie działa, to tutaj można szukać winnego
+        with sd.InputStream(device=4, channels=CHANNEL, callback=self.callback,
+                        blocksize=int(self.sampling_rate * self.timeweighting),
+                        samplerate=self.sampling_rate):
+            while True:
+                sleep(1)
+                if not do_reset_queue.empty():
+                    item = do_reset_queue.get_nowait()
+                    if item==True:
+                        print('Resetting average')
+                        self.l_dbs = []
+                        self.a_dbs = []
+                        do_reset_queue.task_done()
+            
+                lin_npy = np.array(self.l_dbs)
+                a_npy = np.array(self.a_dbs)
+                out_dict = {
+                    "lin_instant":20*np.log10(np.mean(10**(lin_npy[-int(1/self.timeweighting):]/20))),
+                    "lin_minute":20*np.log10(np.mean(10**(lin_npy[-int(60/self.timeweighting):]/20))),
+                    "lin_fifteen_mins":20*np.log10(np.mean(10**(lin_npy[-int(900/self.timeweighting):]/20))),
+                    "a_instant":20*np.log10(np.mean(10**(a_npy[-int(1/self.timeweighting):]/20))),
+                    "a_minute":20*np.log10(np.mean(10**(a_npy[-int(60/self.timeweighting):]/20))),
+                    "a_fifteen_mins":20*np.log10(np.mean(10**(a_npy[-int(900/self.timeweighting):]/20)))
+                }
+                spl_queue.put(out_dict)
+                # print(out_dict)
+# print(sd.query_devices())
+# spl_met = SPL_Meter_Manager({})
+# spl_met.measure_SPL(Queue(), Queue())
---
+++ concert_spl_monitor.py
@@ -0,0 +1,117 @@
+from SPL_meter import SPL_Meter_Manager
+from guizero import App, PushButton, Text, Box
+import threading
+import math
+from queue import Queue, Empty
+
+spl_queue = Queue(5)
+do_reset_queue = Queue(1)
+measurement_manager = SPL_Meter_Manager({})
+
+meas_thread = threading.Thread(target=measurement_manager.measure_SPL, args=(spl_queue,do_reset_queue,), daemon=True)
+meas_thread.start()
+
+color_active = "#00E626"
+color_active_weighing = "#162bfa"
+danger_color = "#ed1e1e"
+
+def set_a():
+    a_button.text_color=color_active_weighing
+    lin_button.text_color="#ffffff"
+
+def set_lin():
+    a_button.text_color="#ffffff"
+    lin_button.text_color=color_active_weighing
+
+
+def set_instant():
+    one_button.text_color="#ffffff"
+    fifteen_button.text_color="#ffffff"
+    instant_button.text_color=color_active
+
+def set_minute():
+    one_button.text_color=color_active
+    fifteen_button.text_color="#ffffff"
+    instant_button.text_color="#ffffff"
+
+def set_fifteen():
+    one_button.text_color="#ffffff"
+    fifteen_button.text_color=color_active
+    instant_button.text_color="#ffffff"
+
+def reset_avg():
+    if do_reset_queue.empty():
+     do_reset_queue.put_nowait(True)
+     text.value=f"{0:.1f}"
+
+def get_data(queue):
+    time=""
+    weigh=""
+
+    if fifteen_button.text_color==color_active: time="fifteen_mins"
+    if one_button.text_color==color_active: time="minute"
+    if instant_button.text_color==color_active: time="instant"
+
+    if a_button.text_color==color_active_weighing: weigh="a"
+    if lin_button.text_color==color_active_weighing: weigh="lin"
+
+    value_to_get = f"{weigh}_{time}"
+    try:
+        item = queue.get_nowait()
+    except Empty:
+        print("empty")
+    else:
+        value = item.get(value_to_get, 0)
+        if math.isnan(value): value=0
+        if weigh=="a" and (time=="fifteen_mins" or time=="minute") and value>=100: text.text_color=danger_color
+        if value<100: text.text_color=color_active
+        if value != 0: text.value=f"{value:.1f}"        
+        queue.task_done()
+
+app = App(title="Concert audio monitor", bg="#000000",  )
+whole_app_box = Box(app, align="bottom", height="fill", width="fill")
+
+level_indic_box = Box(whole_app_box, align="top", width="fill")
+
+top_pad = Box(level_indic_box, height=100, width="fill", align="top")
+
+text_box = Box(level_indic_box, align="top", width="fill")
+
+text = Text(text_box, text="0", color=color_active, align="top",width="fill", size=600)
+text.repeat(1000, lambda: get_data(spl_queue))  
+
+botom_pad = Box(level_indic_box, height=0, width="fill", align="bottom")
+
+buttons_box = Box(whole_app_box, layout="grid")
+
+reset_button = PushButton(buttons_box, command=reset_avg, text="Reset averaging", grid=[0,0], padx=30, pady=30)
+
+instant_button = PushButton(buttons_box, command=set_instant, text="Instant SPL", grid=[1,0], padx=80, pady=30)
+one_button = PushButton(buttons_box, command=set_minute, text="1 minute average", grid=[2,0], padx=30, pady=30)
+fifteen_button = PushButton(buttons_box, command=set_fifteen, text="15 minute average", grid=[3,0], padx=30, pady=30)
+
+spacer_box = Box(buttons_box, width=50, height="fill", grid=[4,0])
+
+a_button = PushButton(buttons_box, command=set_a, text="A weighing", grid=[5,0], padx=30, pady=30)
+lin_button = PushButton(buttons_box, command=set_lin, text="Z weighing", grid=[6,0], padx=30, pady=30)
+
+instant_button.text_color=color_active
+instant_button.text_size=20
+
+one_button.text_color="#ffffff"
+one_button.text_size=20
+
+fifteen_button.text_color="#ffffff"
+fifteen_button.text_size=20
+
+reset_button.text_color="#ffffff"
+reset_button.text_size=20
+
+a_button.text_color=color_active_weighing
+a_button.text_size=20
+
+lin_button.text_color="#ffffff"
+lin_button.text_size=20
+
+app.set_full_screen()
+app.display()
